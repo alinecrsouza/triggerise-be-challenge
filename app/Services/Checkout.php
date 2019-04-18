4e3ac1fd20d9bb64ec6ffb8c0abd9326875bb216
Aline C. R. Souza <linecrsouza@gmail.com>
2019-04-18 18:05:48 +0100

    Create Checkout service

---
+++ app/Services/Checkout.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aline
+ * Date: 18/04/19
+ * Time: 11:39
+ */
+
+namespace App\Services;
+
+
+use App\Product;
+
+class Checkout
+{
+    /**
+     * The pricing rules
+     *
+     * @var array
+     *
+     * Examples:
+     * $pricingRules['TICKET'] = ['rule' => [ 'name' => 'twoForOne']]
+     * $pricingRules['HOODIE'] = ['rule' => ['name' => 'bulk', 'args' => ['minOrder' => 3, 'discount' => 1]]]
+     */
+    private $pricingRules = array();
+
+    /**
+     * @var array
+     */
+    private $items = array();
+
+    /**
+     * Checkout constructor.
+     * @param array $pricingRules
+     */
+    public function __construct(array $pricingRules)
+    {
+        $this->pricingRules = $pricingRules;
+    }
+
+    /**
+     * Scan a product
+     *
+     * @param string $productCode
+     * @throws \Exception
+     */
+    public function scan(string $productCode)
+    {
+        $product = Product::where('code', $productCode)->first();
+
+        if ($product) {
+            if (array_key_exists($productCode, $this->items)) {
+                $this->items[$productCode]['quantity']++;
+            } else {
+                $this->items[$productCode]['quantity'] = 1;
+                $this->items[$productCode]['unitaryPrice'] = $product->price;
+            }
+        } else {
+
+            throw new \Exception('Product not found');
+        }
+    }
+
+    /**
+     * Get the total of the cart
+     */
+    public function total()
+    {
+        $total = 0;
+
+        foreach ($this->items as $key => $item) {
+            if (array_key_exists($key, $this->pricingRules)) {
+                $total += PricingRule::apply($item, $this->pricingRules[$key]['rule']);
+            } else {
+                $total += $item['quantity'] * $item['unitaryPrice'];
+            }
+        }
+
+        return $total;
+    }
+
+    /**
+     * Clear cart
+     */
+    public function clear()
+    {
+        $this->items = [];
+    }
+}
